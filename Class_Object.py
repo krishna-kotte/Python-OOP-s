7d6dd7989569e9e550baf8d972a56a8a95e0e0dd
Krishna <krishna.kotte2005@gmail.com>
2024-09-04 22:33:14 +0530

    Example for Class_Object.py

---
+++ Class_Object.py
@@ -0,0 +1,13 @@
+
+# Class is Created!
+class Voter:
+    def age(self):
+        print("18")
+
+# object is created for Class (Voter)
+obj_Voter = Voter()
+
+# method is called by object
+obj_Voter.age()
+
+print(type(obj_Voter))
